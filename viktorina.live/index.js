aa8a422caf728a9a7b295a405e652a9e9e754609
Aldas <37956008+brutall100@users.noreply.github.com>
2022-12-11 16:30:10 +0200

    Add files via upload
    Pirmas ikelimas

---
+++ viktorina.live/index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+
+// fs.readFile('header.html', (err, data) => {
+//   if (err) throw err;
+
+//   fs.writeFile('index.html', data, (err) => {
+//     if (err) throw err;
+//     console.log('File saved successfully!');
+//   });
+// });
+
+
+
+
